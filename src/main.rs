2627b02f99d0ce790632929ffb1e4c2ea0d66df5
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2026-06-01 15:58:37 +0200

    `main.rs`: Set `wm_class` to `xfce-aero-lang-changer` properly
    I did not use the right API

--- src/main.rs
+++ src/main.rs
@@ -667,7 +667,7 @@ fn main() {
     let theme = WidgetTheme::new(ThemeType::Aero);
     theme.apply();
 
-    app::set_wm_class("xfce-aero-lang-changer", "xfce-aero-lang-changer");
+    Window::set_default_xclass("xfce-aero-lang-changer");
 
     let available = Rc::new(RefCell::new(get_available_locales()));
     let current = get_current_locale();
