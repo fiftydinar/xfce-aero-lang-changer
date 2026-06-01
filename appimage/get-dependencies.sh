6d665f232f32e881c81d0a3ee25c31ca0be35a00
fiftydinar <srbaizoki4@tuta.io>
2026-06-01 12:44:29 +0200

    Build from PKGBUILD using make-aur-package for AppImage

--- appimage/get-dependencies.sh
+++ appimage/get-dependencies.sh
@@ -2,15 +2,4 @@
 
 set -eu
 
-echo "Installing package dependencies..."
-pacman -Syu --noconfirm \
-  rustup \
-  cmake \
-  base-devel \
-  libx11 libxext libxinerama libxcursor libxrender libxfixes libxft \
-  fontconfig pango cairo glib2
-
-rustup default stable
-
-echo "Installing debloated packages..."
 get-debloated-pkgs --add-common --prefer-nano
--- appimage/make-appimage.sh
+++ appimage/make-appimage.sh
@@ -8,8 +8,10 @@ export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 
-# Build the Rust binary
-cargo build --release
+# Build and install from local PKGBUILD
+# Skip integrity check since sha512sums change with each release
+export SKIP_INTEGRITY_CHECK=1
+make-aur-package
 
 # Find system icon
 ICON_PATH=$(find /usr/share/icons -name "preferences-desktop-locale" -type f 2>/dev/null | head -1)
@@ -18,10 +20,10 @@ if [ -z "$ICON_PATH" ]; then
 fi
 
 export ICON="${ICON_PATH:-preferences-desktop-locale}"
-export DESKTOP=./xfce-aero-lang-changer.desktop
+export DESKTOP=/usr/share/applications/xfce-aero-lang-changer.desktop
 
-# Bundle binary and deps into AppDir
-quick-sharun target/release/xfce-aero-lang-changer
+# Bundle binary, deps, desktop, and icon into AppDir
+quick-sharun /usr/bin/xfce-aero-lang-changer
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
